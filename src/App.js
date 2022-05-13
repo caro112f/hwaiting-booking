40cd178ad8785146b8c042a7913195de551cafb4
Caroline Nielsen <caro112f@stud.kea.dk>
2022-05-13 12:01:31 +0200

    burgermenu working

--- src/App.js
+++ src/App.js
@@ -1,34 +1,70 @@
 import { Link, Routes, Route } from "react-router-dom";
 
+import { useRef, useState, useEffect } from "react";
+
+import { gsap } from "gsap";
+
 import Home from "./routes/Home";
-import Step1 from "./routes/Step1";
-import Step2 from "./routes/Step2";
-import Step3 from "./routes/Step3";
-import Step4 from "./routes/Step4";
-import Step5 from "./routes/Step5";
+import Tickets from "./routes/Tickets";
+import CampingSpots from "./routes/CampingSpots";
+import Additional from "./routes/Additional";
+import Information from "./routes/Information";
+import Payment from "./routes/Payment";
 import Congrats from "./routes/Congrats";
 
 function App() {
+  const [menuOpen, setMenuOpen] = useState(false);
+  function ToggleBurgermenu() {
+    setMenuOpen((old) => !old);
+  }
+
+  const menuRef = useRef();
+  useEffect(() => {
+    gsap.from(menuRef.current, { x: 1500 });
+    gsap.to(menuRef.current, { duration: 1, x: 0 });
+  });
+
   return (
     <div className="App">
-      <nav>
-        <ul>
-          <Link to="/Home">Home</Link>
-        </ul>
-      </nav>
+      <section id="navigation">
+        <nav>
+          <div id="logo_wrapper">
+            <img alt="" />
+          </div>
+          <div id="menu" ref={menuRef} className={menuOpen ? null : "inactive"}>
+            <ul>
+              <li>
+                <Link
+                  id="header_link"
+                  className="nav-link"
+                  to="/Step1"
+                  onClick={ToggleBurgermenu}
+                >
+                  <button>BUY TICKETS</button>
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div id="burgermenu" onClick={ToggleBurgermenu}>
+            <span id="bar1" className={menuOpen ? "active1" : null}></span>
+            <span id="bar2" className={menuOpen ? "active2" : null}></span>
+            <span id="bar3" className={menuOpen ? "active3" : null}></span>
+          </div>
+        </nav>
+      </section>
       <section>
         {" "}
-        <Link id="header_link" to="/Step1">
+        <Link id="h1-link" to="/Step1">
           <button>BUY TICKETS</button>
         </Link>
       </section>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="step1" element={<Step1 />} />
-        <Route path="step2" element={<Step2 />} />
-        <Route path="step3" element={<Step3 />} />
-        <Route path="step4" element={<Step4 />} />
-        <Route path="step5" element={<Step5 />} />
+        <Route path="step1" element={<Tickets />} />
+        <Route path="step2" element={<CampingSpots />} />
+        <Route path="step3" element={<Additional />} />
+        <Route path="step4" element={<Information />} />
+        <Route path="step5" element={<Payment />} />
         <Route path="congrats" element={<Congrats />} />
       </Routes>
     </div>
