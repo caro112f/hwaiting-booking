68232ce5481d11d4db057c871114530c5c67b7aa
Caroline Nielsen <caro112f@stud.kea.dk>
2022-05-23 10:01:47 +0200

    available + price

--- src/routes/CampingSpots.jsx
+++ src/routes/CampingSpots.jsx
@@ -1,13 +1,19 @@
 import { Link } from "react-router-dom";
+import Basket from "../components/Basket";
 
 import Spot from "../components/Spot";
 
 export default function Step2(props) {
+  let campPrice = props.dataCamping.map(({ price, ...rest }) => {
+    return price;
+  });
+
   return (
     <section id="campingspots" className="steps">
       <div className="heading-wrapper">
         <h1>Step 2</h1>
         <p>Please pick a camping spot </p>
+        <p>Booking fee: {campPrice[0]} DKK</p>
       </div>
 
       <article className="map">
@@ -24,6 +30,7 @@ export default function Step2(props) {
         </div>
       </article>
 
+      {/* on click event send put request  */}
       <div className="next-step">
         <Link to="/booking/additional">Next</Link>
       </div>
