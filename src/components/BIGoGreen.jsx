71a6f78b39ab4afcfda12da86f328353e0bb677e
Freja <frej400d@stud.kea.dk>
2022-05-23 15:49:34 +0200

    green basket button

--- src/components/BIGoGreen.jsx
+++ src/components/BIGoGreen.jsx
@@ -4,9 +4,11 @@ import { BasketContext } from "../contexts/basket";
 export default function BIGoGreen({ green }) {
   const { setBasket } = useContext(BasketContext);
 
-  function remove(id) {
-    //bruge setBasket
-    //amount -1
+  function remove() {
+    setBasket((oldState) => ({
+      ...oldState,
+      gogreenBA: { ...green, added: false },
+    }));
   }
   return (
     <li>
