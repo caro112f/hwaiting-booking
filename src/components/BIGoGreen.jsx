b37db5982e7f7660aa7470e90e5c7f2126d33ea4
Caroline Nielsen <caro112f@stud.kea.dk>
2022-05-23 14:35:23 +0200

    push to frey

--- src/components/BIGoGreen.jsx
+++ src/components/BIGoGreen.jsx
@@ -1,6 +1,19 @@
+import { useContext } from "react";
+import { BasketContext } from "../contexts/basket";
+
 export default function BIGoGreen({ green }) {
+  const { setBasket } = useContext(BasketContext);
+
+  function remove(id) {
+    //bruge setBasket
+    //amount -1
+  }
   return (
     <li>
+      <button className="basket-minus" onClick={remove}>
+        <p>-</p>
+      </button>
+      <p>{green.amount}</p>
       <p>{green.type}</p>
       <p>{green.price}</p>
     </li>
--- src/components/GoGreen.jsx
+++ src/components/GoGreen.jsx
@@ -7,9 +7,47 @@ export default function GoGreen({ green }) {
   // onclick set "added" to true and display info in basket
   // add remove function that sets "added" to false and automatically removes from basket
 
-  function toggleGreen() {
+  function toggleGreen(props) {
     // fra burgermenu: setMenuOpen((old) => !old);
+    console.log("clicked");
+
+    setBasket((oldState) => ({
+      ...oldState,
+      gogreenBA: { ...props.green, amount: -1 },
+    }));
   }
+
+  // if (basket.gogreenBA.added === false) {
+  //   basket.gogreenBA.added = true;
+  //   setBasket((oldState) => ({
+  //     ...oldState,
+  //     gogreenBA: { ...props.green, amount: 1 },
+  //   }));
+
+  // } else {
+  //   basket.gogreenBA.added = false;
+  //   setBasket((oldState) => ({
+  //     ...oldState,
+  //     gogreenBA: { ...props.green, amount: 0 },
+  //   }));
+  // }
+
+  // {
+  //   green.added = true;
+  //   setBasket((oldState) => ({
+  //     ...oldState,
+  //     gogreenBA: { ...props.green, amount: 1 },
+  //   }));
+  //   console.log("if false then", green.added);
+  // } else {
+  //   green.added = false;
+  //   setBasket((oldState) => ({
+  //     ...oldState,
+  //     gogreenBA: { ...props.green, amount: -1 },
+  //   }));
+  //   console.log("if true then:", green.added);
+  // }
+
   return (
     <div>
       <p>{green.type}</p>
