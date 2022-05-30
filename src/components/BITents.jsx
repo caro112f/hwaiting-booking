03875608a04bc9a26438f3642fa822b81974fec9
Cecilie Jasmin Jørgensen <ceci919p@stud.kea.dk>
2022-05-30 17:35:37 +0200

    test

--- src/components/BITents.jsx
+++ src/components/BITents.jsx
@@ -42,13 +42,26 @@ export default function BITents({ type, price, amount, id, freezeTents }) {
           <div style={freezeTents ? { display: "none" } : { display: "block" }}>
             <button className="basket-minus" onClick={() => remove(id)}>
               <p>-</p>
+
+              <button className="trashcan" onClick={remove}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-trash-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" />
+                </svg>
+              </button>
             </button>
           </div>
-          <p>{amount} </p>
+          <p>{amount} x </p>
           <div style={freezeTents ? { display: "none" } : { display: "block" }}>
-            <button className="basket-plus" onClick={() => buymore(id)}>
+            {/*  <button className="basket-plus" onClick={() => buymore(id)}>
               <p>+</p>
-            </button>
+            </button> */}
           </div>
         </div>
 
